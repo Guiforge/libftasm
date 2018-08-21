576a8a8edb014f849356e35c67c3663bc7e21b82
gpouyat <gpouyat@vejja.fr>
2018-08-21 13:38:06 +0200

    [FIX]Master: remove comment + add gitignore

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gpouyat <gpouyat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/07 15:41:11 by gpouyat           #+#    #+#             */
-/*   Updated: 2018/08/21 12:05:28 by gpouyat          ###   ########.fr       */
+/*   Updated: 2018/08/21 13:31:15 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,58 +28,6 @@ static int	test_ascii(char *name, int (*ft_f)(int), int (*f)(int)) {
 	printf("\n");
 	return (test_nb);
 }
-
-// static int	test_puts() {
-// 	int	test_nb;
-// 	int fd_back;
-
-// 	test_nb = 0;
-// 	printf("---- test ft_puts ---- \n");
-
-
-// 	assert(ft_puts("- test ft_puts") == puts("- test puts"));
-// 	test_nb++;
-// 	ft_puts("√");
-
-// 	fd_back = dup(STDOUT_FILENO);
-// 	close(STDOUT_FILENO);
-// 	assert(ft_puts("- test ft_puts") == puts("- test puts"));
-// 	dup2(fd_back, STDOUT_FILENO);
-// 	close(fd_back);
-// 	test_nb++;
-// 	ft_puts("√");
-
-
-// 	assert(puts(NULL) == ft_puts(NULL));
-// 	test_nb++;
-// 	ft_puts("√");
-
-// 	printf("\n ---- %d tests passed ---- \n\n", test_nb);
-// 	return (test_nb);
-// }
-
-// static int test_strcat() {
-// 	int		test_nb;
-// 	char	buf[9];
-
-// 	test_nb = 0;
-// 	printf("---- test ft_strcat ---- \n");
-
-
-// 	bzero(buf, 9);
-// 	ft_strcat(buf, "");
-// 	ft_strcat(buf, "Bon");
-// 	ft_strcat(buf, "j");
-// 	ft_strcat(buf, "our.");
-// 	ft_strcat(buf, "");
-// 	assert(strcmp(buf, "Bonjour.") == 0);
-// 	assert(buf == ft_strcat(buf, ""));
-// 	test_nb++;
-// 	ft_puts("√");
-
-// 	printf("\n ---- %d tests passed ---- \n\n", test_nb);
-// 	return (test_nb);
-// }
 
 int main() {
 	size_t		test_nb;
