c7b4e44804e229c422576a046a15d27e2f9a67bd
gpouyat <gpouyat@vejja.fr>
2018-10-16 13:41:11 +0200

    [Master] Norme: norme

--- srcs/ft_toupper.s
+++ srcs/ft_toupper.s
@@ -2,23 +2,23 @@ section .text align=16
 global _ft_toupper
 
 _ft_toupper:
-    push rbp
-    mov rbp, rsp
+	push rbp
+	mov rbp, rsp
 
-    cmp rdi, 'a'
-    jl .not_lower
-    cmp rdi, 'z'
-    jg .not_lower
-    sub rdi, 32
-    mov rax, rdi
+	cmp rdi, 'a'
+	jl .not_lower
+	cmp rdi, 'z'
+	jg .not_lower
+	sub rdi, 32
+	mov rax, rdi
 
 	mov rsp, rbp
 	pop rbp
 	ret
 
 .not_lower:
-    mov rax, rdi
+	mov rax, rdi
 
-    mov rsp, rbp
+	mov rsp, rbp
 	pop rbp
 	ret
